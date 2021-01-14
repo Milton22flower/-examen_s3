15ecc8f8d6b4146a00f56fa728e7eced349e383b
Milton22flower <77445608+Milton22flower@users.noreply.github.com>
2021-01-14 11:19:48 -0500

    Add files via upload

---
+++ examen_s3/js/p3.js
@@ -0,0 +1,39 @@
+function mesa1() {
+    console.log("Mesa 1");
+}
+
+function mesa2() {
+    console.log("Mesa 2");
+}
+
+function mesa3() {
+    console.log("Mesa 3");
+}
+
+function mesa4() {
+    console.log("Mesa 4");
+}
+
+function mesa5() {
+    console.log("Mesa 5");
+}
+
+function mesa6() {
+    console.log("Mesa 6");
+}
+
+function mesa7() {
+    console.log("Mesa 7");
+}
+
+function mesa8() {
+    console.log("Mesa 8");
+}
+
+function mesa9() {
+    console.log("Mesa 9");
+}
+
+function mesa10() {
+    console.log("Mesa 10");
+}
---
+++ examen_s3/js/p4.js
@@ -0,0 +1,207 @@
+var automoviles = new Array();
+
+var jeep = new Object;
+jeep.llantas = "peuqeñas"
+jeep.puerta = "turqueza"
+jeep.año = "2011"
+jeep.ventana = "grande"
+jeep.motor = "1000"
+jeep.asientos = "reclinables"
+jeep.color = "amarillo"
+
+var nissan = new Object;
+nissan.llantas = "grandes"
+nissan.puerta = "marrón"
+nissan.año = "2014"
+nissan.ventana = "grande"
+nissan.motor = "1000"
+nissan.asientos = "reclinables"
+nissan.color = "amarillo"
+
+var fiat = new Object;
+fiat.llantas = "grandes"
+fiat.puerta = "ocre"
+fiat.año = "2013"
+fiat.ventana = "grande"
+fiat.motor = "1000"
+fiat.asientos = "reclinables"
+fiat.color = "fucsia"
+
+var peugeot = new Object;
+peugeot.llantas = "grandes"
+peugeot.puerta = "fucsia"
+peugeot.año = "2015"
+peugeot.ventana = "grande"
+peugeot.motor = "1000"
+peugeot.asientos = "no reclinables"
+peugeot.color = "amarillo"
+
+var lexus = new Object;
+lexus.llantas = "grandes"
+lexus.puerta = "ocre"
+lexus.año = "2015"
+lexus.ventana = "grande"
+lexus.motor = "1000"
+lexus.asientos = "reclinables"
+lexus.color = "amarillo"
+
+var toyota = new Object;
+toyota.llantas = "grandes"
+toyota.puerta = "rojo"
+toyota.año = "2015"
+toyota.ventana = "grande"
+toyota.motor = "1000"
+toyota.asientos = "reclinables"
+toyota.color = "amarillo"
+
+var infinit = new Object;
+infinit.llantas = "grandes"
+infinit.puerta = "púrpura"
+infinit.año = "2005"
+infinit.ventana = "grande"
+infinit.motor = "1000"
+infinit.color = "amarillo"
+
+var subaru = new Object;
+subaru.llantas = "grandes"
+subaru.puerta = "rojo"
+subaru.año = "2005"
+subaru.ventana = "grande"
+subaru.motor = "1000"
+subaru.asientos = "reclinables"
+subaru.color = "púrpura"
+
+var suzuki = new Object;
+suzuki.llantas = "grandes"
+suzuki.puerta = "rojo"
+suzuki.año = "2015"
+suzuki.ventana = "grande"
+suzuki.motor = "1000"
+suzuki.asientos = "reclinables"
+suzuki.color = "amarillo"
+
+var dacia = new Object;
+dacia.llantas = "grandes"
+dacia.puerta = "rojo"
+dacia.año = "2015"
+dacia.ventana = "grande"
+dacia.motor = "1000"
+dacia.asientos = "reclinables"
+dacia.color = "amarillo"
+
+var citroen = new Object;
+citroen.llantas = "grandes"
+citroen.puerta = "rojo"
+citroen.año = "2015"
+citroen.ventana = "grande"
+citroen.motor = "1000"
+citroen.asientos = "reclinables"
+citroen.color = "amarillo"
+
+var ford = new Object;
+ford.llantas = "grandes"
+ford.puerta = "rojo"
+ford.año = "2015"
+ford.ventana = "grande"
+ford.motor = "1000"
+ford.asientos = "reclinables"
+ford.color = "amarillo"
+
+var honda = new Object;
+honda.llantas = "grandes"
+honda.puerta = "rojo"
+honda.año = "2015"
+honda.ventana = "grande"
+honda.motor = "1000"
+honda.asientos = "reclinables"
+honda.color = "amarillo"
+
+var chevrolet = new Object;
+chevrolet.llantas = "grandes"
+chevrolet.puerta = "rojo"
+chevrolet.año = "2015"
+chevrolet.ventana = "grande"
+chevrolet.motor = "1000"
+chevrolet.asientos = "reclinables"
+chevrolet.color = "amarillo"
+
+var landrober = new Object;
+landrober.llantas = "grandes"
+landrober.puerta = "rojo"
+landrober.año = "2015"
+landrober.ventana = "grande"
+landrober.motor = "1000"
+landrober.asientos = "reclinables"
+landrober.color = "amarillo"
+
+var hyundai = new Object;
+hyundai.llantas = "grandes"
+hyundai.puerta = "rojo"
+hyundai.año = "2015"
+hyundai.ventana = "grande"
+hyundai.motor = "1000"
+hyundai.asientos = "reclinables"
+hyundai.color = "amarillo"
+
+var porsche = new Object;
+porsche.llantas = "grandes"
+porsche.puerta = "rojo"
+porsche.año = "2015"
+porsche.ventana = "grande"
+porsche.motor = "1000"
+porsche.asientos = "reclinables"
+porsche.color = "amarillo"
+
+var bentley = new Object;
+bentley.llantas = "grandes"
+bentley.puerta = "rojo"
+bentley.año = "2015"
+bentley.ventana = "grande"
+bentley.motor = "1000"
+bentley.asientos = "reclinables"
+bentley.color = "amarillo"
+
+var audi = new Object;
+audi.llantas = "grandes"
+audi.puerta = "rojo"
+audi.año = "2015"
+audi.ventana = "grande"
+audi.motor = "1000"
+audi.asientos = "reclinables"
+audi.color = "amarillo"
+
+var volvo = new Object;
+volvo.llantas = "grandes"
+volvo.puerta = "rojo"
+volvo.año = "2015"
+volvo.ventana = "grande"
+volvo.motor = "1000"
+volvo.asientos = "reclinables"
+volvo.color = "amarillo"
+
+automoviles.push(Audi)
+automoviles.push(Mercedes-Benz)
+automoviles.push(Lexus)
+automoviles.push(Renault)
+automoviles.push(Ford)
+automoviles.push(Opel)
+automoviles.push(Toyota)
+automoviles.push(Tesla)
+automoviles.push(Volvo)
+automoviles.push(Volkswagen)
+automoviles.push(Subaru)
+automoviles.push(Seat)
+automoviles.push(Rolls-Royce)
+automoviles.push(Mini)
+automoviles.push(Lexus)
+automoviles.push(Maserati)
+automoviles.push(Lamborghini)
+automoviles.push(Jaguar)
+automoviles.push(Honda)
+automoviles.push(Mazda)
+
+function mostrar() {
+    alert(automoviles[8].ventana);
+
+
+}
